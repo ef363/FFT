e28f982c8ce171e6ef6bf77e145c97bbf39937b5
David Lingenbrink <d.lingenbrink@gmail.com>
2015-04-10 11:47:23 -0400

    Added a Naive DFT for comparison

--- fft.py
+++ fft.py
@@ -15,11 +15,23 @@ def FFT(X):
 		Y[k+N/2] = evens[k] - Twiddle * odds[k]
 	return Y
 
+
+def DFT(X):
+	#This is a naive implementation of the DFT for testing
+	Y = []
+	N = len(X)
+	for k in range(N):
+		YNew = 0
+		for n in range(N):
+			YNew += X[n]*cmath.exp(-2*cmath.pi*1j*k*n/N)
+		Y+=[YNew]
+	return Y
+
 def test():
-	print "All zeroes ", FFT([0]*16)
-	print "All ones ", FFT([1]*16)
-	print "Alternate -1 and 1 ", FFT([(-1)**k for k in range(16)])
-	print "Exp ", FFT([cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)])
+	print "All zeroes ", FFT([0]*16)==DFT([0]*16)
+	print "All ones ", FFT([1]*16),DFT([1]*16)
+	print "Alternate -1 and 1 ", FFT([(-1)**k for k in range(16)]),DFT([(-1)**k for k in range(16)])
+	print "Exp ", FFT([cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)]),DFT([cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)])
 
 
 test()
