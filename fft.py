4b8ca8d8d1da4ef42a8b84915b1e97711fff818e
David Lingenbrink <d.lingenbrink@gmail.com>
2015-04-17 14:02:53 -0400

    Changed calculation of Twiddle to ensure O(nlogn) running time

--- fft.py
+++ fft.py
@@ -9,10 +9,13 @@ def FFT(X):
 	evens = FFT(X[0::2])
 	odds = FFT(X[1::2])
 	Y = range(N)
+	w = cmath.exp(-2*cmath.pi*1j/N)
+	Twiddle = 1
+	# Twiddle should equal cmath.exp(-2*cmath.pi*k*1j/N) in run k
 	for k in range(N/2):
-		Twiddle = cmath.exp(-2*cmath.pi*k*1j/N)
 		Y[k]=evens[k] + Twiddle * odds[k]
 		Y[k+N/2] = evens[k] - Twiddle * odds[k]
+		Twiddle *= w
 	return Y
 
 
@@ -26,12 +29,3 @@ def DFT(X):
 			YNew += X[n]*cmath.exp(-2*cmath.pi*1j*k*n/N)
 		Y+=[YNew]
 	return Y
-
-#def test():
-#	print "All zeroes ", FFT([0]*16)==DFT([0]*16)
-#	print "All ones ", FFT([1]*16),DFT([1]*16)
-#	print "Alternate -1 and 1 ", FFT([(-1)**k for k in range(16)]),DFT([(-1)**k for k in range(16)])
-#	print "Exp ", FFT([cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)]),DFT([cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)])
-#
-#
-#test()
