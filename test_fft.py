9fc13da3e7e014363414c7187bd022e69766aa6e
David Lingenbrink <d.lingenbrink@gmail.com>
2015-04-17 14:24:45 -0400

    Added a method to check whether entries of vectors are almost equal. Added to unit testing of fft.

--- test_fft.py
+++ test_fft.py
@@ -5,32 +5,53 @@ import unittest
 import cmath
 
 class TestFFT(unittest.TestCase):
-     def testAllZeros(self):
-	  self.assertEqual(fft.FFT([0]*16), fft.DFT([0]*16))
+    def testAllZerosSmall(self):
+        self.assertEqual(fft.FFT([0]*16), fft.DFT([0]*16))
+
+    def testAllOnesSmall(self):
+        self.almostEqualArrays(fft.FFT([1]*16), fft.DFT([1]*16))
+
+    def testAlternatingSmall(self):
+        x = [(-1)**k for k in range(16)]
+        self.almostEqualArrays(fft.FFT(x), fft.DFT(x))
+
+    def testExpSmall(self):
+        e = [cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)]
+        self.almostEqualArrays(fft.FFT(e), fft.DFT(e))
+
+    def testAllZerosBig(self):
+        self.assertEqual(fft.FFT([0]*(2**10)), fft.DFT([0]*(2**10)))
+
+    def testAllOnesBig(self):
+        self.almostEqualArrays(fft.FFT([1]*(2**10)), fft.DFT([1]*(2**10)))
+
+    def testAlternatingBig(self):
+        x = [(-1)**k for k in range((2**10))]
+        self.almostEqualArrays(fft.FFT(x), fft.DFT(x))
+
+    def testExpBig(self):
+        e = [cmath.exp(8j*2*cmath.pi*k/(2**10)) for k in range((2**10))]
+        self.almostEqualArrays(fft.FFT(e), fft.DFT(e))
 
 
-#### The following fail, I think because there is no "assertAlmostEqual" for vectors?
 
-#     def testAllOnes(self):
-#          self.assertEqual(fft.FFT([1]*16), fft.DFT([1]*16))
-
-#     def testAlternating(self):
-#          x = [(-1)**k for k in range(16)]
-#          self.assertEqual(fft.FFT(x), fft.DFT(x))
-
-#     def testExp(self):
-#          e = [cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)]
-#          self.assertEqual(fft.FFT(e), fft.DFT(e))
-
+    def almostEqualArrays(self, v1, v2):
+        self.assertEqual(len(v1), len(v2))
+        for i in range(len(v1)):
+            self.assertAlmostEqual(v1[i],v2[i])
 
 
 
 def suite():
-     suite = unittest.TestSuite()
-     suite.addTest(TestFFT('testAllZeros'))
-#     suite.addTest(TestFFT('testAllOnes'))
-#     suite.addTest(TestFFT('testAlternating'))
-#     suite.addTest(TestFFT('testExp'))
-     return suite
+    suite = unittest.TestSuite()
+    suite.addTest(TestFFT('testAllZerosSmall'))
+    suite.addTest(TestFFT('testAllOnesSmall'))
+    suite.addTest(TestFFT('testAlternatingSmall'))
+    suite.addTest(TestFFT('testExpSmall'))
+    suite.addTest(TestFFT('testAllZerosBig'))
+    suite.addTest(TestFFT('testAllOnesBig'))
+    suite.addTest(TestFFT('testAlternatingBig'))
+    suite.addTest(TestFFT('testExpBig'))
+    return suite
 
 unittest.TextTestRunner(verbosity=2).run(suite())
