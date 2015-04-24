af30dcc368aa2491742083ec5b87f8948549c015
David Lingenbrink <d.lingenbrink@gmail.com>
2015-04-24 14:42:13 -0400

    Made Lists in test_fft into numpy arrays for speed.

--- test_fft.py
+++ test_fft.py
@@ -7,31 +7,31 @@ import numpy as np
 
 class TestFFT(unittest.TestCase):
     def testAllZerosSmall(self):
-        np.testing.assert_allclose(fft.FFT([0]*16), np.fft.fft([0]*16), rtol=0, atol=1e-10)
+        np.testing.assert_allclose(fft.FFT(np.zeros(16)), np.fft.fft(np.zeros(16)), rtol=0, atol=1e-10)
 
     def testAllOnesSmall(self):
-        np.testing.assert_allclose(fft.FFT([1]*16), np.fft.fft([1]*16), rtol=0, atol=1e-10)
+        np.testing.assert_allclose(fft.FFT(np.ones(16)), np.fft.fft(np.ones(16)), rtol=0, atol=1e-10)
 
     def testAlternatingSmall(self):
-        x = [(-1)**k for k in range(16)]
+        x = np.array([(-1)**k for k in range(16)])
         np.testing.assert_allclose(fft.FFT(x), np.fft.fft(x), rtol=0, atol=1e-10)
 
     def testExpSmall(self):
-        e = [cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)]
+        e = np.array([cmath.exp(8j*2*cmath.pi*k/16) for k in range(16)])
         np.testing.assert_allclose(fft.FFT(e), np.fft.fft(e), rtol=0, atol=1e-10)
 
     def testAllZerosBig(self):
-        np.testing.assert_allclose(fft.FFT([0]*(2**10)), np.fft.fft([0]*(2**10)), rtol=0, atol=1e-10)
+        np.testing.assert_allclose(fft.FFT(np.zeros(2**10)), np.fft.fft(np.zeros(2**10)), rtol=0, atol=1e-10)
 
     def testAllOnesBig(self):
-        np.testing.assert_allclose(fft.FFT([1]*(2**10)), np.fft.fft([1]*(2**10)), rtol=0, atol=1e-10)
+        np.testing.assert_allclose(fft.FFT(np.ones(2**10)), np.fft.fft(np.ones(2**10)), rtol=0, atol=1e-10)
 
     def testAlternatingBig(self):
-        x = [(-1)**k for k in range((2**10))]
+        x = np.array([(-1)**k for k in range((2**10))])
         np.testing.assert_allclose(fft.FFT(x), np.fft.fft(x), rtol=0, atol=1e-10)
 
     def testExpBig(self):
-        e = [cmath.exp(8j*2*cmath.pi*k/(2**10)) for k in range((2**10))]
+        e = np.array([cmath.exp(8j*2*cmath.pi*k/(2**10)) for k in range((2**10))])
         np.testing.assert_allclose(fft.FFT(e), np.fft.fft(e), rtol=0, atol=1e-10)
 
 #testing fails here...fixing
@@ -42,21 +42,21 @@ class TestFFT(unittest.TestCase):
 
 class TestDFT(unittest.TestCase):
     def testAllZeros(self):
-        np.testing.assert_allclose([0]*8, fft.DFT([0]*8), rtol=0, atol=1e-10)
+        np.testing.assert_allclose(np.array([0]*8), fft.DFT(np.array([0]*8)), rtol=0, atol=1e-10)
 
     def testAllOnes(self):
-        np.testing.assert_allclose([8]+[0]*7, fft.DFT([1]*8), rtol=0, atol=1e-10)
+        np.testing.assert_allclose(np.array([8]+[0]*7), fft.DFT(np.array([1]*8)), rtol=0, atol=1e-10)
 
     def testAlternating(self):
-        np.testing.assert_allclose([0]*4+[8]+[0]*3, fft.DFT([(-1)**k for k in range(8)]), rtol=0, atol=1e-10)
+        np.testing.assert_allclose(np.array([0]*4+[8]+[0]*3), fft.DFT(np.array([(-1)**k for k in range(8)])), rtol=0, atol=1e-10)
 
     def testExp(self):
-        e = [cmath.exp(2j*2*cmath.pi*k/(8)) for k in range(8)]
-        np.testing.assert_allclose([0]*2+[8]+[0]*5, fft.DFT(e), rtol=0, atol=1e-10)
+        e = np.array([cmath.exp(2j*2*cmath.pi*k/(8)) for k in range(8)])
+        np.testing.assert_allclose(np.array([0]*2+[8]+[0]*5), fft.DFT(e), rtol=0, atol=1e-10)
     
     def testCatastrophicAnnihilationDFT(self):
-	y=[2**55,1,-2**55]
-	np.testing.assert_allclose(np.fft.fft(y), fft.DFT(y))
+	   y=np.array([2**55,1,-2**55])
+	   np.testing.assert_allclose(np.fft.fft(y), fft.DFT(y))
 
     
 suiteFFT = unittest.TestLoader().loadTestsFromTestCase(TestFFT)
