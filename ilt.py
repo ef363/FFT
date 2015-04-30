eb5a40a8ced1878161594d512d9c220160b2b481
Emily <ef363@cornell.edu>
2015-04-30 15:32:50 -0400

    Found lambda and beta, added them to code, fixed minor things

--- ilt.py
+++ ilt.py
@@ -3,8 +3,8 @@
 # Inputs: fhat - output of laplace transform on function f (fhat is a function?)
 #	  delta - step size
 #	  M - a power of 2, length of vector
-#	  lambda - ?
-#	  beta - ?
+#	  lambda - sequence of numbers defined in paper
+#	  beta - same thing as above
 # 	  "backwards" FFT (I'm hoping that means inverse FFT)
 # Output: f(i*delta), i = 0, 1, ..., M-1
 import numpy as np
@@ -14,6 +14,8 @@ def ILT(fhat, delta, M):
 	M2 = 8*M
 	a = 44/M2
 	n = 16
+	lamb = np.array([4.44089209850063e-016, 6.28318530717958, 12.5663706962589, 18.8502914166954, 25.2872172156717, 34.296971663526, 56.1725527716607, 170.533131190126])
+	beta = np.array([1, 1.00000000000004, 1.00000015116847, 1.00081841700481, 1.09580332705189, 2.00687652338724, 5.94277512934943, 54.9537264520382])
 
 	# STEP 1
 	fhat_mat = np.zeros(n/2,M2+1) 
@@ -22,14 +24,14 @@ def ILT(fhat, delta, M):
 	# THIS DOESN'T WORK - don't know what lambda or beta are
 	for k in range(M2+1):
 		beta_sum = 0
-		for j in range(n/2+1)[1:]:
+		for j in range(n/2):
 			# This next line gives an error. Trying to extract real part.
-			fhat_mat(j,k)=fhat((a+1j*lambda(j)+2*1j*np.pi*k/M2)/delta).real
+			fhat_mat[j,k]=fhat((a+1j*lamb(j)+2*1j*np.pi*k/M2)/delta) # later: take the real part of fhat
 			beta_sum += beta(j)*fhat_mat(j,k)
-		fhat_vect(k) = (2/delta)*beta_sum
-		if k=0: beta_sum0 = beta_sum
+		fhat_vect[k] = (2/delta)*beta_sum
+		if k==0: beta_sum0 = beta_sum
 	# at this point, beta_sum is sum with k=M2
-	fhat_vect(0) = (beta_sum + beta_sum0)/delta
+	fhat_vect[0] = (beta_sum + beta_sum0)/delta
 
 	# STEP 2 ## THIS PART IS SUPPOSED TO BE SAME AS BACKWARDS FFT!!!!!
 	fl = np.zeros(M2)
@@ -37,12 +39,12 @@ def ILT(fhat, delta, M):
 		sum  = 0
 		for k in range(M2):
 			sum += fhat_vect(k)*np.cos(2*np.pi*l*k/M2)
-		fl(l) = sum/M2
+		fl[l] = sum/M2
 
 	# STEP 3
 	f = np.zeros(M)
 	for l in range(M):
-		f(l) = np.exp(a*l)*fl(l) # f(l) here is supposed to be f(l*delta) in the function
+		f[l] = np.exp(a*l)*fl(l) # f(l) here is supposed to be f(l*delta) in the function
 
 	return(f)
 # So we just get f(l*delta) out of this, but we can find f(k) at other values of k using quadrature.
